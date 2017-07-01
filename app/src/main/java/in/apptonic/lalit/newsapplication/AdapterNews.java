4e4088464372d5796a87b869091ec3d8291344f4
Lalit Sonawane <appdeveloper.lalit@gmail.com>
2017-07-01 19:13:18 +0530

    initial commit

---
+++ app/src/main/java/in/apptonic/lalit/newsapplication/AdapterNews.java
@@ -0,0 +1,58 @@
+package in.apptonic.lalit.newsapplication;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import in.apptonic.lalit.newsapplication.model.News;
+
+/**
+ * Created by lalitkumarsonawane on 01/07/17.
+ */
+
+public class AdapterNews extends RecyclerView.Adapter<AdapterNews.NewsViewHolder> {
+
+    List<News> newsList = new ArrayList<>();
+
+    public AdapterNews(List<News> newsList) {
+        this.newsList = newsList;
+    }
+
+    @Override
+    public NewsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list, parent, false);
+        NewsViewHolder newsViewHolder = new NewsViewHolder(view);
+        return newsViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(NewsViewHolder holder, int position) {
+
+        holder.newsTitle.setText(newsList.get(position).getTitle());
+        holder.newsDesc.setText(newsList.get(position).getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return newsList.size();
+    }
+
+    public class NewsViewHolder extends RecyclerView.ViewHolder {
+        TextView newsTitle;
+        TextView newsDesc;
+
+        public NewsViewHolder(View itemView) {
+            super(itemView);
+
+            newsTitle = itemView.findViewById(R.id.title);
+            newsDesc = itemView.findViewById(R.id.description);
+
+        }
+    }
+}
--- app/src/main/java/in/apptonic/lalit/newsapplication/MainActivity.java
+++ app/src/main/java/in/apptonic/lalit/newsapplication/MainActivity.java
@@ -1,14 +1,63 @@
 package in.apptonic.lalit.newsapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.ParsedRequestListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import in.apptonic.lalit.newsapplication.model.News;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    RecyclerView.Adapter adapter;
+    List<News> newsList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AndroidNetworking.initialize(getApplicationContext());
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new AdapterNews(newsList);
+
+        downloadNews();
+
+    }
+
+    private void downloadNews() {
+        AndroidNetworking.get("https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey=81aa06e91e5a4f189d1b7a64f07bc373")
+                .setTag(this)
+                .setPriority(Priority.HIGH)
+                .build()
+                .getAsObjectList(News.class, new ParsedRequestListener<List<News>>() {
+
+
+                    @Override
+                    public void onResponse(List<News> response) {
+
+                       
+
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+
+                    }
+                });
 
     }
 }
