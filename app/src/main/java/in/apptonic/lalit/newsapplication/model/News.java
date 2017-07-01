2c57db4601b260a2754c68559e95015c580e7fed
Lalit Sonawane <appdeveloper.lalit@gmail.com>
2017-07-01 18:26:46 +0530

    initial commit

---
+++ app/src/main/java/in/apptonic/lalit/newsapplication/model/News.java
@@ -0,0 +1,32 @@
+package in.apptonic.lalit.newsapplication.model;
+
+/**
+ * Created by lalitkumarsonawane on 01/07/17.
+ */
+
+public class News {
+
+    String title;
+    String description;
+
+    public News(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
